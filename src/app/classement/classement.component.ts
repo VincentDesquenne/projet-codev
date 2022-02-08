8a39e31a6515f98bd61ec90bc19d7a24970ce499
Fabien <goubin.fabien@gmail.com>
2022-02-08 12:44:16 +0100

    add classement

--- src/app/classement/classement.component.ts
+++ src/app/classement/classement.component.ts
@@ -10,12 +10,14 @@ import {ActivatedRoute, Router} from '@angular/router';
 export class ClassementComponent implements OnInit {
   displayedColumns: string[] = ['position', 'pays', 'aqi'];
   classementBest = [];
+  classementBad = [];
   constructor(private statsService: StatsService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
     this.statsService.getClassement().subscribe(
       (classement) => {
         let classTampon = []
+        let classTampon2 = []
         let compteur = 1;
         for (let i = 0; i < 6; i = i + 2) {
           classTampon.push({
@@ -24,7 +26,18 @@ export class ClassementComponent implements OnInit {
             aqi: classement[i + 1],
           })
           compteur++;
+
         }
+        compteur = 1;
+        for (let i = 6; i < 12; i = i + 2) {
+          classTampon2.push({
+            position: compteur,
+            pays: classement[i ],
+            aqi: classement[i + 1],
+          })
+          compteur++;
+        }
+        this.classementBad = classTampon2;
         this.classementBest = classTampon;
 
       }
